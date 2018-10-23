b3f765b5c15e8287e24b52231165949ff08530d3
khmaies hassen <khmaieshassen@hotmail.com>
2018-10-23 17:05:17 +0100

    adding README and code

---
+++ README.md
@@ -0,0 +1,27 @@
+@monster5/log
+[![npm (scoped)]
+(https://img.shields.io/badge/npm-v1.0.0-brightgreen.svg)]
+(https://github.com/khmaies5/log)
+
+make console logs better
+
+
+## Install
+
+```
+$ npm install @monster5/log
+```
+
+## Usage
+
+```js
+const mylog = require("@monster5/log");
+
+mylog('Hello World!')
+mylog('Success!', 'success')
+mylog('Error!', 'error')
+mylog('Warning!', 'warning')
+mylog('Info...', 'info')
+
+
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = function mylog(message, color='black') {
+    switch (color) {
+        case 'success':  
+             color = 'Green'
+             break
+        case 'info':     
+                color = 'Blue'  
+             break;
+        case 'error':   
+             color = 'Red'   
+             break;
+        case 'warning':  
+             color = 'Orange' 
+             break;
+        default: 
+             color = color
+    }
+
+    console.log(`%c${message}`, `color:${color}`)
+};
